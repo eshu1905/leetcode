cd0856f25d0e1fe1b549307421b1cbef16cb62ed
G.easwer <garimellaeshear@gmail.com>
2024-11-04 09:31:08 +0530

    Time: 0 ms (100%), Space: 16.6 MB (37.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,11 +1,17 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        n=len(nums1)-m
-        for i in range(n):
-            nums1.pop()
-        nums1.extend(nums2)
-        nums1.sort()    
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
+        x=m-1
+        y=n-1
+        for z in range(m+n-1,-1,-1):
+            if x<0:
+                nums1[z]=nums2[y]
+                y-=1
+            elif y<0:
+                return 
+            elif nums1[x]>nums2[y]:
+                nums1[z]=nums1[x]
+                x-=1
+            else:
+                nums1[z]=nums2[y]
+                y-=1                 
+       
