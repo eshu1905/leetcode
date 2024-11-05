2a9d87da0deb96857031c678c3bc55562f3c79d8
G.easwer <garimellaeshear@gmail.com>
2024-11-05 18:11:51 +0530

    Time: 551 ms (45.54%), Space: 11.6 MB (72.13%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,19 +1,30 @@
 class Solution(object):
     def fourSum(self, nums, target):
+        nums.sort()
         n=len(nums)
-        st=set()
+        ls=[]
         for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1,n):
-                hashset=set()
-                for k in range(j+1,n):
-                    more=target-(nums[i]+nums[j]+nums[k])
-                    if more in hashset:
-                        temp=[nums[i],nums[j],nums[k],more]
-                        temp.sort()
-                        st.add(tuple(temp))
-                    hashset.add(nums[k])
-
-        ans=list(st)
-        return ans                        
-        
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                k=j+1
+                l=n-1
+                while(k<l):
+                    sum=nums[i]+nums[j]+nums[k]+nums[l]
+                    if sum==target:
+                        temp=[nums[i],nums[j],nums[k],nums[l]]
+                        ls.append(temp)
+                        k+=1
+                        l-=1
+                        while(k<l and nums[k]==nums[k-1]):
+                            k+=1
+                        while(k<l and nums[l]==nums[l+1]):
+                            l-=1
+                    elif sum<target:
+                        k+=1
+                    else:
+                        l-=1
+        return ls                                    
         
