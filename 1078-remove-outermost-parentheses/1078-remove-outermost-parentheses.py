2d88b9b7776420c8bc22688794e71995ebc27b15
G.easwer <garimellaeshear@gmail.com>
2024-11-08 21:48:25 +0530

    Time: 0 ms (100%), Space: 16.7 MB (63.09%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        result=""
+        balence=0
+        for i in s:
+            if i=="(":
+                if balence>0:
+                    result+=i
+                balence+=1
+            elif i==")":
+                balence-=1
+                if balence>0:
+                    result+=i
+        return result                                
+
+
+            
+
+
+        
