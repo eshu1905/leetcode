691ad24958ef0fc0aae61e9df3fa47a4bc346a4a
G.easwer <garimellaeshear@gmail.com>
2024-11-20 15:02:14 +0530

    Time: 0 ms (100%), Space: 16.7 MB (54.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        subsets=1<<n
-        ans=[]
-        for i in range(subsets):
-            temp=[]
-            for j in range(n):
-                if i&(1<<j):
-                    temp.append(nums[j])
-            ans.append(temp)
-        return ans           
+        def backtrack(index, current):
+        # Base case: if we've considered all elements
+            if index == len(nums):
+                result.append(current[:])  # Append a copy of the current subset
+                return
+        
+        # Include the current element
+            current.append(nums[index])
+            backtrack(index + 1, current)
+        
+        # Exclude the current element (backtrack)
+            current.pop()
+            backtrack(index + 1, current)
+    
+        result = []
+        backtrack(0, [])
+        return result
 
         
