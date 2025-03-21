5c6183d38021a02eea8968f6b146abc9ab6e8e34
G.easwer <garimellaeshear@gmail.com>
2025-03-21 11:54:09 +0530

    Time: 20 ms (97.93%), Space: 26.5 MB (30.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -3,13 +3,8 @@ class Solution(object):
         hmap={}
         for i in nums:
             if i in hmap:
-                hmap[i]+=1
-            else:
-                hmap[i]=1
-        for i,n in hmap.items():
-            if n>1:
                 return i
-        return None        
+            hmap[i]=1           
 
         """
         :type nums: List[int]
