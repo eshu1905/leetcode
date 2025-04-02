f122c5d5cb980dba109951a3fb645e0bc86e9ae7
G.easwer <garimellaeshear@gmail.com>
2025-04-02 11:32:30 +0530

    Time: 0 ms (100%), Space: 13.4 MB (33.29%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def search(self, nums, target):
-        n=len(nums)
         low=0
-        high=n-1
-        while(low<=high):
+        high=len(nums)-1
+        while low<=high:
+
             mid=(low+high)//2
             if nums[mid]==target:
                 return mid
@@ -13,3 +13,5 @@ class Solution(object):
                 low=mid+1
         return -1                
         
+
+       
